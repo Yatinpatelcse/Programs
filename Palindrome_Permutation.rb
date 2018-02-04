611bb7b87943f7b591475278e59bda73af2b2ebc
DESKTOP-821155Q\yatin <yatinpatelcse@gmail.com>
2018-02-04 11:52:18 -0800

    completed palindrome_permutation

--- Palindrome_Permutation.rb
+++ Palindrome_Permutation.rb
@@ -2,4 +2,30 @@
 #
 # For example,
 # "code" -> False, "aab" -> True, "carerac" -> True.
-#
+
+## No Reference
+
+def is_palindrome(str)
+
+  if str.nil?
+    fail("String is Empty\n")
+  end
+
+  hash_table = {}
+  (0..str.length - 1).each do |i|
+    if hash_table[str[i]]
+      hash_table.delete (str[i])
+    else
+      hash_table[str[i]] = 1
+    end
+  end
+  if hash_table.count > 1
+    return false
+  else
+    return true
+  end
+end
+
+str = "carerac"
+result = is_palindrome(str)
+print result
