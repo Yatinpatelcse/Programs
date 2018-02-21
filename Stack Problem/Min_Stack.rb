b1a0f1a26a804f4caa18a6413a1a53bc25819701
DESKTOP-821155Q\yatin <yatinpatelcse@gmail.com>
2018-02-20 22:36:42 -0800

    MIn Stack completed

---
+++ Stack Problem/Min_Stack.rb
@@ -0,0 +1,51 @@
+class MinStack
+
+  UnderflowError = Class.new StandardError
+
+  attr_accessor :min_stacks
+
+  def initialize
+    @min_stacks = []
+  end
+
+  def push(x)
+    @min_stacks.push x
+  end
+
+  def pop()
+    fail UnderflowError if @min_stacks.empty?
+    @min_stacks.pop
+  end
+
+  def top()
+    @min_stacks.last
+  end
+
+  def get_min()
+    fail UnderflowError if @min_stacks.empty?
+    @min_stacks.min
+  end
+end
+
+
+obj = MinStack.new()
+obj.push(6)
+obj.push(7)
+obj.push(4)
+obj.push(3)
+obj.push(5)
+obj.pop()
+param_3 = obj.top()
+puts "top => #{param_3}"
+param_4 = obj.get_min()
+puts "Min => #{param_4}"
+obj.pop()
+param_3 = obj.top()
+puts "top => #{param_3}"
+param_4 = obj.get_min()
+puts "Min => #{param_4}"
+obj.pop()
+param_3 = obj.top()
+puts "top => #{param_3}"
+param_4 = obj.get_min()
+puts "Min => #{param_4}"
